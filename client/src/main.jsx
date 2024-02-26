b3ea7ee812813c24d0a56524da7d796fc97e0ac8
Robert Bailey <r.bail@hotmail.com>
2024-02-26 19:20:52 +1030

    trying to deploy properly

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
+import React from "react";
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 
 import App from './App.jsx'
 import Home from './pages/Home'
